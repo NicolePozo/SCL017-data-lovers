193b91da538b560e306868cb6601fbbf96ce3a27
NicolePozo <nicolepozo.salas@gmail.com>
2021-06-10 16:10:29 -0400

    pintando consola

--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+/*flip cards*/
+//const cards = document.querySelectorAll('.flip-cards');
+
+/*function flipCard() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));*/
+
+const pokemon = data.pokemon 
+for(let i=0; i <pokemon.lenght; i++){
+    console.log(pokemon.size[i].height);
+    console.log(pokemon.size[i].weight);
+}
+
+console.log(pokemon[0].size, pokemon[0].weight);
+
+
+//console.log(example, data);
